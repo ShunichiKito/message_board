73262cfcaeed286c3ebd45129f178593270fd9d1
shunichi.kito <shunichi.kito@rakuten.com>
2018-05-24 01:58:47 +0000

    message-model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laaaa
                 </div>
 
                 <div class="links">
